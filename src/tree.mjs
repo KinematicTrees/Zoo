d9a549bf04047be48ff88f9ea6206835e4b5f24a
bowspw <stuart.wilson@bow.ltd>
2025-12-27 11:53:44 +0000

    meshes

--- src/tree.mjs
+++ src/tree.mjs
@@ -193,7 +193,7 @@ function formatMesh(mesh,color,id){
             o.userData.index = id
             o.material = o.userData.resetMaterial;
             o.material = mat;
-            //o.geometry.computeVertexNormals();
+            o.geometry.computeVertexNormals();
         }
     });
     mesh.scene.rotateX(Math.PI/2);
